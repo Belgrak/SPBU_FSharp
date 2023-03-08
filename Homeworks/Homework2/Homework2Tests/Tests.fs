1b6e3e473959bb700de7698fe544e7168cf66d50
Gleb <glekar03@mail.ru>
2023-03-08 10:44:03 +0300

    dev: add task 3 with tests

--- Homeworks/Homework2/Homework2Tests/Tests.fs
+++ Homeworks/Homework2/Homework2Tests/Tests.fs
@@ -3,6 +3,7 @@ module Homework2Tests
 open NUnit.Framework
 open EvenCounter
 open BinaryTree
+open ArithmeticTreeEval
 open FsCheck
 
 [<Test>]
@@ -28,7 +29,31 @@ let TestMapForTree () =
             && checkEquality firstRight secondRight func
         | _ -> false
 
-    let areSame binaryTree testFunc =
+    let testMap binaryTree testFunc =
         checkEquality binaryTree (mapForTree binaryTree testFunc) testFunc
 
-    Check.QuickThrowOnFailure areSame
+    Check.QuickThrowOnFailure testMap
+
+
+[<Test>]
+let TestArithmeticTreeEval () =
+    let plusTest a b =
+        eval (ArithmeticTree(Plus, Leaf a, Leaf b)) = a + b
+
+    let minusTest a b =
+        eval (ArithmeticTree(Minus, Leaf a, Leaf b)) = a - b
+
+    let multiplyTest a b =
+        eval (ArithmeticTree(Multiply, Leaf a, Leaf b)) = a * b
+
+    let complexSumTest a b c =
+        eval (ArithmeticTree(Plus, ArithmeticTree(Multiply, Leaf a, Leaf b), Leaf c)) = a * b + c
+
+    let complexMultiplyTest a b c =
+        eval (ArithmeticTree(Multiply, Leaf c, ArithmeticTree(Plus, Leaf a, Leaf b))) = c * (a + b)
+
+    Check.QuickThrowOnFailure plusTest
+    Check.QuickThrowOnFailure minusTest
+    Check.QuickThrowOnFailure multiplyTest
+    Check.QuickThrowOnFailure complexSumTest
+    Check.QuickThrowOnFailure complexMultiplyTest
