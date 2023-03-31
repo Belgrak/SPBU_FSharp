e613d41b5990c90df03092b22924cb5178ed637e
Belgrak <glekar03@mail.ru>
2023-03-31 23:31:44 +0300

    dev: add task1, 2 realisations

---
+++ Homeworks/Homework4/H4Task2/Program.fs
@@ -0,0 +1,11 @@
+﻿module PointFree
+
+let func x l = List.map (fun y -> y * x) l
+
+let func1 x = List.map (fun y -> y * x)
+
+let func2 x = List.map ((*) x)
+
+let func3 = List.map << (*)
+
+printf $"{func3 -1 [0; -1]}"
