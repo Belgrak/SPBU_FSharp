a75a118536a2137e665071c22264ca86f3faf108
Belgrak <glekar03@mail.ru>
2023-04-28 12:51:59 +0300

    hotfix: format code

--- Homeworks/Test1/Task2/Library.fs
+++ Homeworks/Test1/Task2/Library.fs
@@ -3,8 +3,14 @@
 let drawRhombus n =
     let rec generate k =
         if abs k = n then
-            String.init (n - 1) (fun i -> " ") + "*\n" + (if k < 0 then "" else generate (k - 1))
+            String.init (n - 1) (fun i -> " ")
+            + "*\n"
+            + (if k < 0 then "" else generate (k - 1))
         else
-            String.init ((abs k) - 1) (fun i -> " ") + "*" + String.init ((n - (abs k)) * 2 - 1) (fun i -> " ") + "*\n" +
-            (if k = 1 then generate -2 else generate (k - 1))
+            String.init ((abs k) - 1) (fun i -> " ")
+            + "*"
+            + String.init ((n - (abs k)) * 2 - 1) (fun i -> " ")
+            + "*\n"
+            + (if k = 1 then generate -2 else generate (k - 1))
+
     generate n
--- Homeworks/Test1/Tests/UnitTest1.fs
+++ Homeworks/Test1/Tests/UnitTest1.fs
@@ -5,9 +5,8 @@ open Supermap
 open Rhombus
 
 
-let f() = printf $"{drawRhombus 4}"
+let f () = printf $"{drawRhombus 4}"
 
 [<Test>]
 let ``first task example`` () =
-    Assert.AreEqual([sin 1.0; cos 1.0; sin 2.0; cos 2.0; sin 3.0; cos 3.0], supermap [sin; cos] [1.0; 2.0; 3.0])
-    
+    Assert.AreEqual([ sin 1.0; cos 1.0; sin 2.0; cos 2.0; sin 3.0; cos 3.0 ], supermap [ sin; cos ] [ 1.0; 2.0; 3.0 ])
