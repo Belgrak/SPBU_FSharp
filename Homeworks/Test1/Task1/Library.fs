eeb206ae65e9fce4587df0f6357e83bc933d8d5e
Belgrak <glekar03@mail.ru>
2023-04-28 12:43:38 +0300

    add task1,2 realisation

---
+++ Homeworks/Test1/Task1/Library.fs
@@ -0,0 +1,4 @@
+﻿module Supermap
+
+let supermap functions arguments =
+    List.concat (List.map (fun x -> (List.map (fun f -> f x) functions)) arguments)
---
+++ Homeworks/Test1/Task3/Library.fs
@@ -0,0 +1,19 @@
+﻿module Option
+
+type binOp = 
+  | Add 
+  | Sub 
+  | Mul 
+  | Div
+type expr = 
+  | Var of string 
+  | Const of int 
+  | BinOp of binOp * expr * expr
+
+
+type OptionBuilder() =
+  member _.Bind(v, f) = Option.bind f v
+  member _.Return v = Some v
+  member _.Zero () = None
+
+let opt = OptionBuilder()
